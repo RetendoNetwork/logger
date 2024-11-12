9457f97370c7a7e68e021ca9b0e59eb6346396a9
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-12 21:03:07 +0100

    Add files via upload

--- logger.py
+++ logger.py
@@ -1,122 +1,91 @@
-import logging
 import os
-import sys
+import logging
 import time
-import inspect
 from colorama import Fore, Back, Style, init
+from pathlib import Path
+import inspect
 
-# Initialize colorama
 init(autoreset=True)
 
-# Log prefixes with colors
-critical_prefix = "CRITICAL"
-critical_prefix_colored = Fore.WHITE + Back.RED + Style.BRIGHT + critical_prefix
+flags = 'a'
+log_template = "[{}] [{}] {} [{} {}:{}] : {}"
 
-error_prefix = "ERROR"
-error_prefix_colored = Fore.RED + Style.BRIGHT + error_prefix
+prefixes = {
+    "critical": {"plain": "CRITICAL", "colored": Fore.WHITE + Back.RED + Style.BRIGHT + "CRITICAL"},
+    "error": {"plain": "ERROR", "colored": Fore.RED + Style.BRIGHT + "ERROR"},
+    "warning": {"plain": "WARNING", "colored": Fore.YELLOW + Style.BRIGHT + "WARNING"},
+    "success": {"plain": "SUCCESS", "colored": Fore.GREEN + Style.BRIGHT + "SUCCESS"},
+    "info": {"plain": "INFO", "colored": Fore.CYAN + Style.BRIGHT + "INFO"},
+}
 
-warning_prefix = "WARNING"
-warning_prefix_colored = Fore.YELLOW + Style.BRIGHT + warning_prefix
+class Logger:
+    def __init__(self, log_folder_root="."):
+        log_folder_path = Path(log_folder_root) / "log"
+        log_folder_path.mkdir(parents=True, exist_ok=True)
 
-success_prefix = "SUCCESS"
-success_prefix_colored = Fore.GREEN + Style.BRIGHT + success_prefix
+        self.all_logs_file = open(log_folder_path / "all.log", flags)
+        self.critical_log_file = open(log_folder_path / "critical.log", flags)
+        self.error_log_file = open(log_folder_path / "error.log", flags)
+        self.warning_log_file = open(log_folder_path / "warning.log", flags)
+        self.success_log_file = open(log_folder_path / "success.log", flags)
+        self.info_log_file = open(log_folder_path / "info.log", flags)
 
-info_prefix = "INFO"
-info_prefix_colored = Fore.CYAN + Style.BRIGHT + info_prefix
+    def _log_line(self, message, prefix, prefix_colored, log_file):
+        file, line, function, package_name = self._get_caller_info()
+        date = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
 
-log_template = "[{date}] [{prefix}] {func} {package} {file}:{line} : {message}"
+        spacing = " " * (len(prefixes["critical"]["plain"]) - len(prefix) + 1)
 
-max_prefix_length = len(critical_prefix)
+        log_plain = log_template.format(date, prefix, " ", function, package_name, line, message)
+        log_plain_spaced = log_template.format(date, prefix, spacing, function, package_name, line, message)
 
+        print(log_template.format(Fore.LIGHTBLACK_EX + date, prefix_colored, spacing, Fore.MAGENTA + function, Fore.GREEN + package_name, Fore.GREEN + file, Fore.YELLOW + line, Style.BRIGHT + message))
 
-def create_log_file(path):
-    try:
-        log_dir = os.path.dirname(path)
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-        return open(path, 'a')
-    except Exception as e:
-        print(f"Error creating log file: {e}")
-        return None
+        log_file.write(log_plain + "\n")
+        self.all_logs_file.write(log_plain_spaced + "\n")
 
+    def _get_caller_info(self):
+        frame = inspect.currentframe().f_back.f_back.f_back
+        filename = os.path.basename(frame.f_code.co_filename)
+        line = frame.f_lineno
+        function = frame.f_code.co_name
+        package_name = frame.f_globals["__name__"]
+        return filename, str(line), function + "()", package_name
 
-def log_line(message, prefix, prefix_colored, log_file):
-    date = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
-    frame = inspect.stack()[2]
-    file = os.path.basename(frame[1])
-    line = frame[2]
-    func = frame[3]
-    package = frame[0].f_globals["__name__"]
+    def critical(self, message):
+        self._log_line(message, prefixes["critical"]["plain"], prefixes["critical"]["colored"], self.critical_log_file)
 
-    prefix_length = len(prefix)
-    spacing = " " * (max_prefix_length - prefix_length + 1)
+    def error(self, message):
+        self._log_line(message, prefixes["error"]["plain"], prefixes["error"]["colored"], self.error_log_file)
 
-    log_plain = log_template.format(date=date, prefix=prefix, func=func, package=package, file=file, line=line, message=message)
-    log_plain_spaced = log_template.format(date=date, prefix=prefix, func=func, package=package, file=file, line=line, message=message)
+    def warning(self, message):
+        self._log_line(message, prefixes["warning"]["plain"], prefixes["warning"]["colored"], self.warning_log_file)
 
-    if colorama_supported():
-        print(log_template.format(date=Fore.LIGHTBLACK_EX + date, prefix=prefix_colored, func=Fore.MAGENTA + "func " + Fore.CYAN + func, package=Fore.GREEN + package, file=Fore.GREEN + file, line=Fore.YELLOW + str(line), message=Style.BRIGHT + message))
-    else:
-        print(log_plain_spaced)
+    def success(self, message):
+        self._log_line(message, prefixes["success"]["plain"], prefixes["success"]["colored"], self.success_log_file)
 
-    log_file.write(log_plain + '\n')
-    log_file.flush()
+    def info(self, message):
+        self._log_line(message, prefixes["info"]["plain"], prefixes["info"]["colored"], self.info_log_file)
 
+    def criticalf(self, message, *args):
+        self.critical(message.format(*args))
 
-def colorama_supported():
-    try:
-        return sys.stdout.isatty() and os.environ.get('TERM') != 'dumb'
-    except Exception:
-        return False
+    def errorf(self, message, *args):
+        self.error(message.format(*args))
 
+    def warningf(self, message, *args):
+        self.warning(message.format(*args))
 
-def critical(message):
-    log_line(message, critical_prefix, critical_prefix_colored, critical_log_file)
+    def successf(self, message, *args):
+        self.success(message.format(*args))
 
+    def infof(self, message, *args):
+        self.info(message.format(*args))
 
-def error(message):
-    log_line(message, error_prefix, error_prefix_colored, error_log_file)
-
-
-def warning(message):
-    log_line(message, warning_prefix, warning_prefix_colored, warning_log_file)
-
-
-def success(message):
-    log_line(message, success_prefix, success_prefix_colored, success_log_file)
-
-
-def info(message):
-    log_line(message, info_prefix, info_prefix_colored, info_log_file)
-
-
-def criticalf(message, *args):
-    log_line(message % args, critical_prefix, critical_prefix_colored, critical_log_file)
-
-
-def errorf(message, *args):
-    log_line(message % args, error_prefix, error_prefix_colored, error_log_file)
-
-
-def warningf(message, *args):
-    log_line(message % args, warning_prefix, warning_prefix_colored, warning_log_file)
-
-
-def successf(message, *args):
-    log_line(message % args, success_prefix, success_prefix_colored, success_log_file)
-
-
-def infof(message, *args):
-    log_line(message % args, info_prefix, info_prefix_colored, info_log_file)
-
-# Log files setup
-log_folder_root = "."
-log_folder_path = os.path.join(log_folder_root, "log")
-log_flags = os.O_APPEND | os.O_CREATE | os.O_WRONLY
-
-all_log_file = create_log_file(os.path.join(log_folder_path, "all.log"))
-critical_log_file = create_log_file(os.path.join(log_folder_path, "critical.log"))
-error_log_file = create_log_file(os.path.join(log_folder_path, "error.log"))
-warning_log_file = create_log_file(os.path.join(log_folder_path, "warning.log"))
-success_log_file = create_log_file(os.path.join(log_folder_path, "success.log"))
-info_log_file = create_log_file(os.path.join(log_folder_path, "info.log"))
+    def close(self):
+        self.all_logs_file.close()
+        self.critical_log_file.close()
+        self.error_log_file.close()
+        self.warning_log_file.close()
+        self.success_log_file.close()
+        self.info_log_file.close()
---
+++ start.bat
@@ -0,0 +1,3 @@
+rm -rf __pycache__
+python3 test.py 
+pause
